70e57928384a992123478da8270eb2d8cdd5bdb3
Streinge <olesof@mail.ru>
2022-11-19 23:10:12 +0500

    other_bot

--- proba.py
+++ proba.py
@@ -1,9 +1,9 @@
 import time
 from telegram.ext import Updater
-chat_id = '-829440234'
-updater = Updater("5520994198:AAFkYOFAi6rNdvLiM6hfYVbYU59OYD3b-3c",
+chat_id = '1082504319'
+updater = Updater("5579939015:AAFJq9Sz0TSZp-NFxRr9_K8QE9Z7dsshymY",
                    use_context=True)
-message = "Полина! Не обращай внимание - это тестирование"
+message = "Не обращай внимание - это тестирование"
 for i in range(10):
     updater.bot.send_message(chat_id=chat_id, text=message)
     time.sleep(10)
